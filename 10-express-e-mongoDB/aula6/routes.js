91c99926a2ab793c2d7cfc607b22c601309c87f6
ruanSignori <cleosignori@gmail.com>
2021-10-15 11:52:11 -0300

    msater

--- 10-express-e-mongoDB/aula6/routes.js
+++ 10-express-e-mongoDB/aula6/routes.js
@@ -2,15 +2,21 @@ const express = require('express');
 const route = express.Router();
 const loginController = require('./src/controllers/loginController');
 const homeController = require('./src/controllers/homeController');
+const contatoController = require('./src/controllers/contatoController');
+
+const { loginRequired } = require('./src/middlewares/middleware');
 
 //Rotas da home
 route.get('/', homeController.index);
 
 //Rotas de login
-
 route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
-route.get('/login/logout', loginController.logout)
+route.get('/login/logout', loginController.logout);
+
+//Rotas de contato
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
 
 module.exports = route;
