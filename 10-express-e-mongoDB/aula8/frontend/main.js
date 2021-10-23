f4c5f2053ae08b7f5b4bac780b64e8624fa89f81
ruanSignori <cleosignori@gmail.com>
2021-10-23 09:41:30 -0300

    master

--- 10-express-e-mongoDB/aula8/frontend/main.js
+++ 10-express-e-mongoDB/aula8/frontend/main.js
@@ -1,12 +1,17 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import Login from './modules/Login';
+import Contato from './modules/Contato';
 
+//Login e cadastro
 const login = new Login('.form-login');
 const cadastro = new Login('.form-cadastro');
 
 login.init();
 cadastro.init();
 
+//Criação de contato
+const contato = new Contato('.form-agenda');
+contato.init();
 
 
