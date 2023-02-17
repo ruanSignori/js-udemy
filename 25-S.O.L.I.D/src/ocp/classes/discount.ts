d3f2e38cf84f57e959b197cf9c956f85023f35b5
ruanSignori <cleosignori@gmail.com>
2023-02-17 12:14:32 -0300

    refactor: changed method  Discount

--- 25-S.O.L.I.D/src/ocp/classes/discount.ts
+++ 25-S.O.L.I.D/src/ocp/classes/discount.ts
@@ -1,23 +1,17 @@
 export abstract class Discount {
-  abstract calculate(value: number): number;
+  protected discount = 0;
+
+  calculate(price: number): number {
+    return price - price * (this.discount * 100);
+  }
 }
 
 export class FiftyPercentDiscount extends Discount {
-  private readonly discount = 50;
-  calculate(price: number): number {
-    return price - price * (this.discount * 100);
-  }
+  protected readonly discount = 50;
 }
 
 export class TenPercentDiscount extends Discount {
-  private readonly discount = 50;
-  calculate(price: number): number {
-    return price - price * (this.discount * 100);
-  }
+  protected readonly discount = 50;
 }
 
-export class NoDiscount extends Discount {
-  calculate(price: number): number {
-    return price;
-  }
-}
+export class NoDiscount extends Discount {}
