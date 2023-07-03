a4e8ae4f197bf1e0c3e7dca1000ebcc921974fb5
ruanSignori <cleosignori@gmail.com>
2023-07-03 06:51:26 -0300

    aula sobre LSP - Liskov Substituiton Principle

---
+++ 25-S.O.L.I.D/src/lsp/classes/order.ts
@@ -0,0 +1,29 @@
+import { Messaging } from '../services/messaging';
+import { Persistency } from './persistency';
+import { OrderStatusCart } from './protocols/order-status';
+import { ShoppingCart } from './shopping-cart';
+
+export class Order {
+  private _orderStatus: OrderStatusCart = 'open';
+  constructor(
+    private readonly cart: ShoppingCart,
+    private readonly messaging: Messaging,
+    private readonly persistency: Persistency,
+  ) {}
+
+  get orderStatus(): Readonly<OrderStatusCart> {
+    return this._orderStatus;
+  }
+
+  checkout(): void {
+    if (this.cart.isEmpty()) {
+      return console.log('Seu carrinho está vazio');
+    }
+
+    this.messaging.sendMessage(
+      `Seu pedido com total de R$${this.cart.total()}foi recebido`,
+    );
+    this.persistency.saveOrder();
+    this.cart.clearItems();
+  }
+}
---
+++ 25-S.O.L.I.D/src/lsp/classes/protocols/order-status.ts
@@ -0,0 +1,1 @@
+export type OrderStatusCart = 'open' | 'closed';
