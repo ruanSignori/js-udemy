1730cb4752c175c13c78f61443021c4130667d76
ruanSignori <cleosignori@gmail.com>
2023-07-11 06:35:17 -0300

    aula sobre spyOn - jest

--- 26-Testes-com-jest/src/services/peristency.spec.ts
+++ 26-Testes-com-jest/src/services/peristency.spec.ts
@@ -1,6 +1,32 @@
-describe('test', () => {
-  it('should return one', () => {
-    const num = 1;
-    expect(num).toBe(1);
+import { Persistency } from './persistency';
+
+describe('Persistency', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it('should return undefined', () => {
+    //Sysrtem under test
+    const sut = new Persistency();
+
+    //Verificar se o retorno da saveOrder foi undefined
+    expect(sut.saveOrder()).toBe(undefined);
+  });
+  it('should call console.log once', () => {
+    const sut = new Persistency();
+
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+
+    //Verificar se o console.log foi executado somente uma vez
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+  it('should call console.log with "Pedido salvo com sucesso"', () => {
+    const sut = new Persistency();
+
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+
+    //Verificar se o conole.log tinha o valor "Pedido salvo com sucesso"
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso');
   });
 });
