e3348eb18eee5d6eedf120a9e2e7657d516216a1
ruanSignori <cleosignori@gmail.com>
2023-07-19 06:09:07 -0300

    test integração de discount

--- 26-Testes-com-jest/src/classes/shopping-cart.spec.ts
+++ 26-Testes-com-jest/src/classes/shopping-cart.spec.ts
@@ -60,4 +60,18 @@ describe('ShoppingCart', () => {
     sut.removeItem(1);
     expect(sut.items.length).toBe(1);
   });
+  it('should call discount.calculate once when totalWithDiscount is called', () => {
+    const { sut, discountMock } = createSutWithProducts();
+
+    const discountMockSpy = jest.spyOn(discountMock, 'calculate');
+    sut.totalWithDiscount();
+    expect(discountMockSpy).toHaveBeenCalledTimes(1);
+  });
+  it('should call discount.calculate with total price once when totalWithDiscount is called', () => {
+    const { sut, discountMock } = createSutWithProducts();
+
+    const discountMockSpy = jest.spyOn(discountMock, 'calculate');
+    sut.totalWithDiscount();
+    expect(discountMockSpy).toHaveBeenCalledWith(sut.total());
+  });
 });
