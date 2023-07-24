78a236378a2032f689e4f57615734fbbc3eaccef
ruanSignori <cleosignori@gmail.com>
2023-07-24 06:45:13 -0300

    editado estilos

--- 27-react-ts-promodoro-app/src/app/page.tsx
+++ 27-react-ts-promodoro-app/src/app/page.tsx
@@ -4,7 +4,12 @@ import { PomodoroTimer } from "@/components";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <PomodoroTimer pomodoroTimer={1500} />
+      <PomodoroTimer
+        pomodoroTimer={3}
+        cycles={3}
+        longRestTime={10}
+        shortRestTime={13}
+      />
     </main>
   );
 }
--- 27-react-ts-promodoro-app/src/components/PomodoroTimer.tsx
+++ 27-react-ts-promodoro-app/src/components/PomodoroTimer.tsx
@@ -5,19 +5,25 @@ import { useInterval } from "@/hooks";
 import { useState } from "react";
 
 type PomodoroTimerProps = {
-  /**O valor atribuído é convertido para segundos */
+  /**Tempo em minutos que a atividade vai durar */
   pomodoroTimer: number;
+
+  /**Tempo em minutos de descanso entre cada Pomodoro */
+  shortRestTime: number;
+  longRestTime: number;
+  cycles: number;
 };
 
 export function PomodoroTimer({ pomodoroTimer }: PomodoroTimerProps) {
-  const [mainTime, setMainTime] = useState(pomodoroTimer);
+  const [mainTime, setMainTime] = useState(pomodoroTimer * 60);
 
   useInterval(() => {
     setMainTime(mainTime - 1);
   }, 1);
   return (
     <div>
-      <h2>Pomodoro {secondsToTime(mainTime)}</h2>
+      <h2>Pomodoro</h2>
+      <p className="timer">{secondsToTime(mainTime)}</p>
       <button className="btn">Eae</button>
     </div>
   );
