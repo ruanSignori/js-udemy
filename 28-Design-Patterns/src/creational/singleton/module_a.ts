dc681e2b0c34d4e0fe50dfcc2cb200317ae89612
ruanSignori <cleosignori@gmail.com>
2023-08-02 08:43:00 -0300

    singleton immediately invoked function

--- 28-Design-Patterns/src/creational/singleton/module_a.ts
+++ 28-Design-Patterns/src/creational/singleton/module_a.ts
@@ -1,7 +1,7 @@
-import { MyDatabaseModule } from './db/my-database-model';
+import { MyDatabaseFunction } from './db/my-database-function';
 import { myDbModuleB } from './module_b';
 
-const myDbModule = MyDatabaseModule;
+const myDbModule = MyDatabaseFunction;
 myDbModule.add({ name: 'ruan', age: 30 });
 myDbModule.add({ name: 'FERNANDO', age: 30 });
 
--- 28-Design-Patterns/src/creational/singleton/module_b.ts
+++ 28-Design-Patterns/src/creational/singleton/module_b.ts
@@ -1,6 +1,6 @@
-import { MyDatabaseModule } from './db/my-database-model';
+import { MyDatabaseFunction } from './db/my-database-function';
 
-const myDbModuleB = MyDatabaseModule;
+const myDbModuleB = MyDatabaseFunction;
 myDbModuleB.add({ name: 'eu sou o B', age: 30 });
 myDbModuleB.add({ name: 'eu também sou o B', age: 30 });
 
